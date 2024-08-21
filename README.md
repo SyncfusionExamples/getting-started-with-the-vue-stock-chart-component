47afcd3d9e53d90145a95cb739b9fd67a6471b7d
Niluba <139118395+Niluba@users.noreply.github.com>
2024-08-21 16:40:50 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# getting-started-with-the-vue-stock-chart-component
+# Getting Started with the Vue Stock Chart Component
+
 A quick start project that shows how to create and configure the Syncfusion Vue Stock Chart component in a Vue app. This sample contains simple code customization, as well as some important features like enabling tooltips and crosshairs.
+ 
+Refer to the following documentation to learn about the Vue Stock Chart component: 
+https://ej2.syncfusion.com/vue/documentation/stock-chart/vue-3-getting-started 
+
+Check out this online example of the Vue Stock Chart component:
+https://ej2.syncfusion.com/vue/demos/#/material/stock-chart/default.html 
+
+## Project prerequisites
+Make sure that you have the compatible versions of [Visual Studio Code](https://code.visualstudio.com/download ) and [NodeJS](https://nodejs.org/en/download) or later version in your machine before starting to work on this project.
+
+## How to run this application
+To run this application, you need to first clone the `getting-started-with-the-vue-stock-chart-component` repository and then open it in Visual Studio Code. Now, simply build and run your project to view the output.
---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import {StockChartComponent as EjsStockchart, CandleSeries,
+StockChartSeriesCollectionDirective as EStockchartSeriesCollection,
+StockChartSeriesDirective as EStockchartSeries, 
+DateTime, LineSeries, SplineSeries, HiloOpenCloseSeries,
+HiloSeries, RangeAreaSeries,Trendlines, EmaIndicator,
+RsiIndicator, BollingerBands, TmaIndicator,
+MomentumIndicator, SmaIndicator, AtrIndicator, 
+AccumulationDistributionIndicator, MacdIndicator, 
+StochasticIndicator, Export, Crosshair, Tooltip
+} from '@syncfusion/ej2-vue-charts';
+import {chartData} from './data';
+import {provide} from 'vue';
+provide('stockChart',[CandleSeries,DateTime, LineSeries, 
+SplineSeries, HiloOpenCloseSeries, HiloSeries, 
+RangeAreaSeries,Trendlines, EmaIndicator,
+RsiIndicator, BollingerBands, TmaIndicator,
+MomentumIndicator, SmaIndicator, AtrIndicator, 
+AccumulationDistributionIndicator, MacdIndicator, 
+StochasticIndicator, Export, Crosshair, Tooltip]);
+const crosshair = {enable: true, lineType: 'Both'};
+const primaryXAxis = {
+  crosshairTooltip: {enable: true}
+};
+const tooltip = {enable: true};
+const enableSelector = false;
+const periods = [{
+  selected: true,
+  intervalType: 'Hours',
+  interval: 12,
+  text: '12H'
+}];
+const enablePeriodSelector = false;
+</script>
+
+<template>
+<ejs-stockchart height="500px" width="900px"
+title="Stock Price Analysis" :crosshair="crosshair"
+:primaryXAxis="primaryXAxis" :tooltip="tooltip"
+:enableSelector="enableSelector" :periods="periods"
+:enablePeriodSelector="enablePeriodSelector">
+<e-stockchart-series-collection>
+  <e-stockchart-series type="Candle" :dataSource="chartData"
+  xName="x">
+  </e-stockchart-series>
+</e-stockchart-series-collection>
+</ejs-stockchart>
+</template>
+
+<style scoped>
+
+</style>
